f6c0e141f92a3655e1a6aed121b30954bf0f5053
Jericho <perilecho@gmail.com>
2023-11-27 19:24:54 -0800

    pl0x pl0x

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -9,7 +9,12 @@ export default function Intro () {
                 if u want to learn more about me, go thru my website duhhh &gt;.&gt;
                 <br></br>
                 <br></br>
-                <b>btw u can resize the windows, go to bottom right of them and drag :P</b>
+                <b>btw u can resize the windows dimension, go to bottom right of them and drag.
+                    also can drag the window bars around the desktop! :P
+                </b>
+                <br></br>
+                <br></br>
+                <b>play around!!</b>
             </p>
         </div>
     )
